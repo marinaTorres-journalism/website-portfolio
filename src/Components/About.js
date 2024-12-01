739587997072e3e4a2af97ac5e58e2a326a23dc1
Konstantin Ivanov <ivanov.konstantin.ict@gmail.com>
2024-12-01 16:09:22 +0000

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Fade from "react-reveal";
+
+class About extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const profilepic = "images/" + this.props.data.image;
+    const bio = this.props.data.bio;
+    const email = this.props.data.email;
+    const resumeDownload = this.props.data.resumedownload;
+    const github = this.props.data.github;
+
+
+    return (
+      <section id="about">
+        <Fade duration={1000}>
+          <div className="row">
+            <div className="three columns">
+              <img
+                className="profile-pic"
+                src={profilepic}
+                alt="Konstantin Ivanov Profile Pic"
+              />
+            </div>
+            <div className="nine columns main-col">
+              <h2>About Me</h2>
+
+              <p>{bio}</p>
+              <div className="row">
+                <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+                    <span>{name}</span>
+                    <br />
+                    <span>{email}</span>
+                  </p>
+                </div>
+                <div className="columns download">
+                  <p>
+                    <a href={resumeDownload} className="button">
+                      <i className="fa fa-download"></i>Download Resume
+                    </a>
+                    <a href={github} className="button btn github-btn">
+                      <i className="fa fa-github"></i>Github
+                    </a>  
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default About;
